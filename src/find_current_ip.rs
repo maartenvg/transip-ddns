c71ff649db7259a4ada508df16a1b116174876f6
Maarten van Grootel <git@vangrootel.org>
2024-10-01 10:54:33 -0400

    Clean up find_current_ip

--- src/find_current_ip.rs
+++ src/find_current_ip.rs
@@ -1,13 +1,12 @@
-use std::f32::consts::PI;
-
 pub fn run() -> Result<String, reqwest::Error> {
-    let mut address = String::new();
     let source = "https://whatismyip.akamai.com/";
 
     let response = reqwest::blocking::get(source)?;
 
-    match response.error_for_status() {
-        Ok(result) => return result.text(),
+    let address = match response.error_for_status() {
+        Ok(result) => result.text(),
         Err(e) =>   return Err(e),
     };
+
+    return address;
 }
